b979eed20f2ce17a1e67c740d23d0f4e63389604
Egbewatt Nkongho <Kabanajawat@gmail.com>
2025-05-27 03:48:51 +0100

    Update READme file

--- project_sql/3_top_demanded_skills.sql
+++ project_sql/3_top_demanded_skills.sql
@@ -1,10 +1,10 @@
 /*
-Question: What are the most in-demannd skills for data analysts?
+Question: What are the most in-demannd skills for data Engineer?
 - join job postings to inner join table similar to query 2
 - Identify the top 5 in-demand skills for data engineer.
 - focus on all job postings.
-- Why? Retrieves the top 5 skills with the highestt demand in the jb market,
-    providing insights inot the valuable skills for jobb seekers.
+- Why? Retrieves the top 5 skills with the highestt demand in the job market,
+    providing insights into the valuable skills for jobb seekers.
 */
 
 SELECT 
